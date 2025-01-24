a4125ec8e69af40a7516c51797d1d0677d92f009
AndKrogh <anders.krogh99@gmail.com>
2025-01-24 11:46:58 +0100

    Making realations between models

---
+++ BudgetTracker.core/Models/ExpenseForecast.cs
@@ -0,0 +1,16 @@
+﻿namespace BudgetTracker.core.Models
+{
+    public class ExpenseForecast
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string ForecastName { get; set; }
+        public DateTime ForecastDate { get; set; }
+        public decimal PredictedAmount { get; set; }
+        public string Notes { get; set; }
+        public int BudgetId { get; set; }
+
+        public User User { get; set; }
+        public Budget Budget { get; set; }
+    }
+}
