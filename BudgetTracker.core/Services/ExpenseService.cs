cc237cd80648382f9172f8e0d255bf3e5e9a8968
AndKrogh <anders.krogh99@gmail.com>
2025-01-26 10:29:38 +0100

    making repositories and services

--- BudgetTracker.core/Services/ExpenseService.cs
+++ BudgetTracker.core/Services/ExpenseService.cs
@@ -1,6 +1,35 @@
-﻿namespace BudgetTracker.core.Services
+﻿using BudgetTracker.core.Models;
+using BudgetTracker.core.Repositories;
+
+namespace BudgetTracker.core.Services
 {
-    internal class ExpenseService
+    public class ExpenseService
     {
+        private readonly BudgetRepository _budgetRepository;
+
+        public ExpenseService(BudgetRepository budgetRepository)
+        {
+            _budgetRepository = budgetRepository;
+        }
+
+        public async Task<IEnumerable<BudgetEntry>> GetExpensesByBudgetIdAsync(int budgetId)
+        {
+            return await _budgetRepository.GetBudgetEntriesAsync(budgetId);
+        }
+
+        public async Task<bool> AddExpenseAsync(BudgetEntry expense)
+        {
+            return await _budgetRepository.AddBudgetEntryAsync(expense);
+        }
+
+        public async Task<bool> UpdateExpenseAsync(BudgetEntry expense)
+        {
+            return await _budgetRepository.UpdateBudgetEntryAsync(expense);
+        }
+
+        public async Task<bool> DeleteExpenseAsync(int expenseId)
+        {
+            return await _budgetRepository.DeleteBudgetEntryAsync(expenseId);
+        }
     }
 }
