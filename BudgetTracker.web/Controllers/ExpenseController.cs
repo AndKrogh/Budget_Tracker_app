03c8f2c360eaba9442b45acb66f3fa07770f22bb
AndKrogh <anders.krogh99@gmail.com>
2025-01-27 15:02:20 +0100

    adding expensecontroller logic

--- BudgetTracker.web/Controllers/ExpenseController.cs
+++ BudgetTracker.web/Controllers/ExpenseController.cs
@@ -1,6 +1,31 @@
-﻿namespace BudgetTracker.web.Controllers
+﻿using BudgetTracker.core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BudgetTracker.web.Controllers
 {
-    public class ExpenseController
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExpenseController : ControllerBase
     {
+        private readonly ExpenseService _expenseService;
+
+        public ExpenseController(ExpenseService expenseService)
+        {
+            _expenseService = expenseService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetExpensesByBudgetIdAsync(int id)
+        {
+            var expense = await _expenseService.GetExpensesByBudgetIdAsync(id);
+            if (expense == null)
+            {
+                return NotFound(new { Message = $"expense with id {id} not found" });
+            }
+
+            return Ok(expense);
+        }
     }
 }
