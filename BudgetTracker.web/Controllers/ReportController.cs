32cd66771291afaf9ac2e1a15da833c10d8f2430
AndKrogh <anders.krogh99@gmail.com>
2025-01-27 15:39:47 +0100

    Making ReportController

--- BudgetTracker.web/Controllers/ReportController.cs
+++ BudgetTracker.web/Controllers/ReportController.cs
@@ -1,6 +1,29 @@
-﻿namespace BudgetTracker.web.Controllers
+﻿using BudgetTracker.core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetTracker.web.Controllers
 {
-    public class ReportController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReportController : ControllerBase
     {
+        private ReportService _reportService;
+
+        public ReportController(ReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GenerateReportAsync(int budgetId, int userId)
+        {
+            var report = await _reportService.GenerateReportAsync(budgetId, userId);
+            if (report == null)
+            {
+                return NotFound(new { Message = $"Report with budgetId {budgetId} and userId {userId} not able to be generated");
+
+            }
+            return Ok(report);
+        }
     }
 }
